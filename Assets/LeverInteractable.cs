f19e1f36ead4a49ec305adfc9476e41e5629ae36
leseeger <leon.seeger@hs-osnabrueck.de>
2024-10-24 16:02:12 +0200

    Add LeverInteractable script to handle lever activation and deactivation events based on angle threshold.

---
+++ Assets/LeverInteractable.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Events;
+
+public class LeverInteractable : MonoBehaviour
+{
+    [SerializeField] private float angleThreshold = 45f;
+    
+    [SerializeField] private UnityEvent onLeverActivated = new UnityEvent();
+    [SerializeField] private UnityEvent onLeverDeactivated = new UnityEvent();
+    
+    private HingeJoint hingeJoint;
+    private bool isActivated = false;
+    private bool wasOverThreshold = false;
+
+    public UnityEvent OnLeverActivated => onLeverActivated;
+    public UnityEvent OnLeverDeactivated => onLeverDeactivated;
+
+    private void Awake()
+    {
+        hingeJoint = GetComponent<HingeJoint>();
+        if (hingeJoint == null)
+        {
+            Debug.LogError("HingeJoint component is required on the same GameObject!");
+        }
+    }
+
+    private void Update()
+    {
+        // Variante 1: Verwendung des HingeJoint
+        float leverAngle = hingeJoint.angle;
+        bool isOverThreshold = Mathf.Abs(leverAngle) > angleThreshold;
+        
+        
+        if (isOverThreshold != wasOverThreshold)
+        {
+            isActivated = isOverThreshold;
+            
+            if (isActivated)
+            {
+                onLeverActivated.Invoke();
+            }
+            else
+            {
+                onLeverDeactivated.Invoke();
+            }
+        }
+        
+        wasOverThreshold = isOverThreshold;
+    }
+}
