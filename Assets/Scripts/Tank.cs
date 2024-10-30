c5d4cc4d1fc255282908cdfdcf77a9fe79a3feec
HSOSOli <oliver.snopko@hs-osnabrueck.de>
2024-10-30 13:53:01 +0100

    Things

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -8,12 +8,24 @@ public class Tank : MonoBehaviour
 {
     [SerializeField] private GameObject tankHole;
     [SerializeField] private UnityEvent fueled = new UnityEvent();
+    private bool canSnap = false;
+    private Vector3 snapPos;
+
+    void FixedUpdate()
+    {
+        if (canSnap)
+        {
+            transform.position = snapPos;
+        }
+    }
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 7)
         {
             fueled.Invoke();
+            snapPos = other.transform.position;
+            canSnap = true;
         }
             
     }
