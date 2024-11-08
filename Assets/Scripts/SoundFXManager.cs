a40b0575e91be5e8bb424d192845c876cbdd0745
Lui <145705607+LouisDiekmann@users.noreply.github.com>
2024-11-08 13:00:05 +0100

    basic audio/ui setup

--- Assets/Scripts/SoundFXManager.cs
+++ Assets/Scripts/SoundFXManager.cs
@@ -1,16 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundFXManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static SoundFXManager instance;
+    [SerializeField] private AudioSource soundFXObject;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySoundFX(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
+        
+        float clipLength = audioSource.clip.length;
+        Destroy(audioSource.gameObject, clipLength);
+        
+    }
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
