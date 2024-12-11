c3cd8c55b7242174a4205988d25523224bed0534
leseeger <leon.seeger@hs-osnabrueck.de>
2024-12-11 17:47:44 +0100

    ad a gamestate

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,34 @@ public class GameMenuManager : MonoBehaviour
     [SerializeField] private float menuDistance;
     [SerializeField] private GameObject menu;
     public InputActionProperty showMenuButton;
-    
+    bool menuActive = true;
+
+    private void Awake()
+    {
+        GameManager.OnGameStateChanged += GameManagerOnOnGameStateChanged;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnGameStateChanged -= GameManagerOnOnGameStateChanged;
+    }
+
+    private void GameManagerOnOnGameStateChanged(GameManager.GameState obj)
+    {
+        if (obj== GameManager.GameState.Menu)
+        {
+            menuActive = true;
+        }
+    }
+
     void Update() {
-        if (showMenuButton.action.WasPressedThisFrame()) {
-            menu.SetActive(!menu.activeSelf);
+        if (showMenuButton.action.WasPressedThisFrame() || menuActive) {
+            //menu.SetActive(!menu.activeSelf);
+            Debug.Log($"GameMenuManager : {menuActive}");
             menu.transform.position = head.position + new Vector3(head.forward.x, head.forward.y, head.forward.z).normalized * menuDistance;
         }
         menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
         menu.transform.forward *= -1;
+        menuActive = false;
     }
 }
