6b1c6d9554736f776e4d3c1439c40397644d98fc
Arpad-H <131017236+Arpad-H@users.noreply.github.com>
2024-12-11 17:46:13 +0100

    lights on off

--- Assets/Scripts/ElectricictyManager.cs
+++ Assets/Scripts/ElectricictyManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Video;
@@ -14,6 +15,14 @@ public class ElectricictyManager : MonoBehaviour
     private List<Light> lights = new List<Light>();
     private GameObject tv;
     private VideoPlayer Videoplayer;
+    
+    [Header("Faerie Lights")] 
+    public List<GameObject> faerieLights;
+    public Material faerieLightsMaterialOn;
+    public Material faerieLightsMaterialOff;
+    [Header("other Lights")] 
+    public List<GameObject> fluorescentLights;
+    public Material fluorescentLightsMaterialOn;
     private void Awake()
     {
         
@@ -39,6 +48,7 @@ public class ElectricictyManager : MonoBehaviour
 
     private void Start()
     {
+        
         findAllLights();
         tv = GameObject.Find("TV Demo");
         if (tv != null)
@@ -50,9 +60,7 @@ public class ElectricictyManager : MonoBehaviour
         {
             Debug.LogError("[ElectricityManager] GamObject tv is null");
         }
-        {
-            
-        }
+       
     }
 
 
@@ -74,8 +82,25 @@ public class ElectricictyManager : MonoBehaviour
         foreach (Light light in lights)
         {
             light.enabled = false;
-            Debug.Log($"[ElectricityManager] Light {light.name} is enabled");
         }
+        
+        faerieLightsMaterialOn.SetInt("Emission" , 0);
+        // foreach (GameObject faerieLight in faerieLights)
+        // {
+        //     MeshRenderer[] meshRenderers = faerieLight.GetComponentsInChildren<MeshRenderer>();
+        //     meshRenderers = meshRenderers.Skip(1).ToArray();
+        //     foreach (MeshRenderer mr in meshRenderers )
+        //     {
+        //         mr.material = faerieLightsMaterialOff;
+        //     }
+        // }
+        //
+        // foreach (GameObject fluorescentLight in fluorescentLights)
+        // {
+        //     // fluorescentLight.GetComponentInChildren<MeshRenderer>().material = fluorescentLightsMaterialOff;
+        // }
+     
+        
     }
 
     private void enableAllLights()
@@ -85,6 +110,20 @@ public class ElectricictyManager : MonoBehaviour
             light.enabled = true;
             Debug.Log($"[ElectricityManager] Light {light.name} is enabled");
         }
+        faerieLightsMaterialOn.SetInt("Emission" , 1);
+        // foreach (GameObject faerieLight in faerieLights)
+        // {
+        //     MeshRenderer[] meshRenderers = faerieLight.GetComponentsInChildren<MeshRenderer>();
+        //     meshRenderers = meshRenderers.Skip(1).ToArray();
+        //     foreach (MeshRenderer mr in  meshRenderers)
+        //     {
+        //         mr.material = faerieLightsMaterialOn;
+        //     }
+        // }
+        // foreach (GameObject fluorescentLight in fluorescentLights)
+        // {
+        //     fluorescentLight.GetComponentInChildren<MeshRenderer>().material = fluorescentLightsMaterialOn;
+        // }
     }
 
 
