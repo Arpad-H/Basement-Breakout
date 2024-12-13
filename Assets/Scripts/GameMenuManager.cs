0e0c0c7332530999abb0a90442a30f1638685247
leseeger <leon.seeger@hs-osnabrueck.de>
2024-12-13 15:52:20 +0100

    The start menu is now displayed parallel to the camera plane with a variable distance in the viewing direction.

--- Assets/Scripts/GameMenuManager.cs
+++ Assets/Scripts/GameMenuManager.cs
@@ -28,14 +28,23 @@ public class GameMenuManager : MonoBehaviour
             menuActive = true;
         }
     }
+    //TODO: richtigen Zeitpunkt finden, wann der Spieler auch wirklich das menü sieht
+    private void OnEnable()
+    {
+        menuActive = true;
+    }
+
 
     void Update() {
         //TODO: Funktiniert nicht
         if (showMenuButton.action.WasPressedThisFrame() || menuActive) {
             //menu.SetActive(!menu.activeSelf);
             Debug.Log($"GameMenuManager : {menuActive}");
-            menu.transform.position = head.position + new Vector3(head.forward.x, head.forward.y, head.forward.z).normalized * menuDistance;
+           // menu.transform.position = head.position + new Vector3(head.forward.x, head.forward.y, head.forward.z).normalized * menuDistance;
+             menu.transform.position = head.position + head.forward * menuDistance;
         }
+        
+      
         menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
         menu.transform.forward *= -1;
         menuActive = false;
