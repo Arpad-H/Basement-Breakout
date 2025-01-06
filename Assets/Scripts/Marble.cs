c31dfb3596db91080ed098605f657ebd651c700c
HSOSOli <oliver.snopko@hs-osnabrueck.de>
2025-01-06 13:35:28 +0100

    Marble Maze is feature complete

--- Assets/Scripts/Marble.cs
+++ Assets/Scripts/Marble.cs
@@ -2,16 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class Marble : MonoBehaviour
 {
     [SerializeField] private Transform startTransform;
-    
-    private void OnTriggerExit(Collider other)
+    [SerializeField] private Transform centerTransform;
+
+    public void Update()
     {
-        if (other.gameObject.layer != 28)
+        if (centerTransform == null)
         {
-            //transform.position = startTransform.position;
+            Destroy(gameObject);
+        }
+        float length = (new Vector2(centerTransform.position.x, centerTransform.position.z) - new Vector2(transform.position.x, transform.position.z)).magnitude;
+        print(length);
+        if (length > 0.2f)
+        {
+            transform.position = startTransform.position;
         }
     }
 }
--- Assets/Scripts/MarbleTrigger.cs
+++ Assets/Scripts/MarbleTrigger.cs
@@ -1,26 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MarbleTrigger : MonoBehaviour
 {
-    [SerializeField] private GameObject door;
-    [SerializeField] private GameObject self;
+    [SerializeField] private GameObject deletable;
     [SerializeField] private GameObject battery;
+    [SerializeField] private GameObject oldBattery;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 27)
         {
             Destroy(gameObject);
-            if (door != null)
+            if (deletable != null)
             {
-                Destroy(door);
+                Destroy(deletable);
             }
-            if (self != null)
+            if (oldBattery != null)
             {
-                Instantiate(battery, self.transform.position, self.transform.rotation);
-                Destroy(self);
+                Instantiate(battery, oldBattery.transform.position, oldBattery.transform.rotation);
+                Destroy(oldBattery);
             }
         }
     }
