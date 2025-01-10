ff2878046eed34977e7e02c8e37e056d53542a72
leseeger <leon.seeger@hs-osnabrueck.de>
2025-01-10 17:45:37 +0100

    WinCollision.cs simplified and Door opens outwards

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : MonoBehaviour
         TVBehavior.gameStateChangedTVBehavior += UpdateGameState;
         CustomCollisionEventHandler.GameStateChangedCostomCollisionEventHandler += UpdateGameState;
         PlayerManager.GameStateChangedPlayer += UpdateGameState;
+        WinCollision.GameStateChangedWinCollision += UpdateGameState;
     }
 
     private void OnDestroy()
@@ -36,6 +37,7 @@ public class GameManager : MonoBehaviour
         TVBehavior.gameStateChangedTVBehavior -= UpdateGameState;
         CustomCollisionEventHandler.GameStateChangedCostomCollisionEventHandler -= UpdateGameState;
         PlayerManager.GameStateChangedPlayer -= UpdateGameState;
+        WinCollision.GameStateChangedWinCollision -= UpdateGameState;
     }
 
     private void Start()
--- Assets/Scripts/WinCollision.cs
+++ Assets/Scripts/WinCollision.cs
@@ -1,48 +1,20 @@
+using System;
 using UnityEngine;
 
 public class WinCollision : MonoBehaviour
 {
-    [SerializeField] private GameObject player; // Spieler-Referenz
-    private GameManager gameManager;
-
-    private void Awake()
-    {
-        gameManager = FindObjectOfType<GameManager>();
-        if (gameManager == null)
-        {
-            Debug.LogError("WinCollision: GameManager not found!");
-        }
-    }
+    [SerializeField] private GameObject targetGameObject;
+    private GameManager gameManager; 
+    public static event Action<GameManager.GameState> GameStateChangedWinCollision;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Überprüfen, ob das kollidierende Objekt das Tag "Door" hat
-        if (other.CompareTag("door"))
+        if (other.gameObject == targetGameObject)
         {
-            Debug.Log("WinCollision: Triggered by an object with the tag 'Door'!");
-
-            // GameState auf Win setzen
-            if (gameManager != null)
-            {
-                gameManager.UpdateGameState(GameManager.GameState.Win);
-                Debug.Log("WinCollision: GameState set to Win.");
-            }
-            else
-            {
-                Debug.LogError("WinCollision: GameManager is missing!");
-            }
-
-            // Spielerposition ändern
-            if (player != null)
-            {
-                player.transform.position = new Vector3(50, 10, 0);
-                Debug.Log("WinCollision: Player position set to (50, 10, 0).");
-            }
-            else
-            {
-                Debug.LogError("WinCollision: Player GameObject is not assigned!");
-            }
-            Debug.Log("WinCollision: Player has won the game!");
+            Debug.Log($"[WinCollision[ {other.gameObject.name} || {targetGameObject.name}");
+            GameStateChangedWinCollision?.Invoke(GameManager.GameState.Win);
+            
         }
+
     }
 }
