97b451c9bd81aaf09164844a57e3a9d4d08e385c
HSOSOli <oliver.snopko@hs-osnabrueck.de>
2025-01-15 17:24:38 +0100

    Lock change to Numpadlock beginning

--- Assets/Scripts/Lock.cs
+++ Assets/Scripts/Lock.cs
@@ -8,28 +8,58 @@ public class Lock : MonoBehaviour
     [SerializeField] private GameObject door;
     [SerializeField] private GameObject tankDummy;
     [SerializeField] private GameObject tank;
-    private GameObject key;
+    [SerializeField] private GameObject leftHand;
+    [SerializeField] private GameObject rightHand;
+    //private GameObject key;
+    private const int code = 0451;
     
     void Start()
     {
         tank.SetActive(false);
-        key = GameObject.Find("KeyInLock");
-        key.SetActive(false);
+        //key = GameObject.Find("KeyInLock");
+        //key.SetActive(false);
         //door.GetComponent<Grabbable>().enabled = false;
         door.SetActive(false);
+        leftHand.SetActive(false);
+        rightHand.SetActive(false);
+    }
+
+    private void unlocked()
+    {
+        door.SetActive(true);
+        tank.SetActive(true);
+        //key.SetActive(true);
+        //door.GetComponent<Grabbable>().enabled = true;
+        Destroy(tankDummy);
+        GetComponent<Rigidbody>().isKinematic = false;
+        //Destroy(other);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 28)
+        if (other.gameObject.layer == 21)
         {
-            door.SetActive(true);
-            tank.SetActive(true);
-            key.SetActive(true);
-            //door.GetComponent<Grabbable>().enabled = true;
-            Destroy(tankDummy);
-            GetComponent<Rigidbody>().isKinematic = false;
-            Destroy(other.gameObject);
+            leftHand.SetActive(true);
+            
+        }
+
+        if (other.gameObject.layer == 22)
+        {
+            rightHand.SetActive(true);
+            
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 21)
+        {
+            leftHand.SetActive(false);
+        }
+
+        if (other.gameObject.layer == 22)
+        {
+            rightHand.SetActive(false);
         }
     }
 }
