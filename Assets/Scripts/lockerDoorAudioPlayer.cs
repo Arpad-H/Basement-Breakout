b131a32cf3b5c6402230e9ceed30ce29ec11aa19
Lui <145705607+LouisDiekmann@users.noreply.github.com>
2025-02-10 09:53:41 +0100

    fuse box haptics and sound

--- Assets/Scripts/lockerDoorAudioPlayer.cs
+++ Assets/Scripts/lockerDoorAudioPlayer.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Oculus.Haptics;
 using UnityEngine;
 
 public class lockerDoorAudioPlayer : MonoBehaviour
 {
-    private float previousYRotation = 0;
-    private float currentYRotation = 0;
+    private float previousRotation = 0;
+    private float currentRotation = 0;
     [SerializeField] private AudioSource[] squeakSound;
     private float squeakTimer = 2.5f;
     private bool justSqueaked = false;
+    [SerializeField] private bool xAxisCheck = false;
+    [SerializeField] private HapticClip switchHaptic;
+    private HapticClipPlayer switchHapticPlayer;
 
     private int frameCounter = 30;
+    
+    private void Start() {
+        switchHapticPlayer = new HapticClipPlayer(switchHaptic);
+    }
     void Update() {
         if (frameCounter > 0) frameCounter--;
         if (justSqueaked) squeakTimer -= Time.deltaTime;
@@ -19,8 +27,10 @@ public class lockerDoorAudioPlayer : MonoBehaviour
             squeakTimer = 5f;
         }
         
-        currentYRotation = transform.rotation.eulerAngles.y;
-        if (currentYRotation - previousYRotation != 0) {
+        if (!xAxisCheck) currentRotation = transform.rotation.eulerAngles.y;
+        else currentRotation = transform.rotation.eulerAngles.x;
+        
+        if (currentRotation - previousRotation != 0) {
             bool squeaking = false;
             
             foreach (var sound in squeakSound) {
@@ -30,13 +40,14 @@ public class lockerDoorAudioPlayer : MonoBehaviour
             }
 
             if (!squeaking && !justSqueaked && frameCounter == 0) {
+                switchHapticPlayer.Play(Controller.Both);
                 AudioSource audio = squeakSound[Random.Range(0, squeakSound.Length - 1)];
                 audio.pitch = Random.Range(0.6f, 1.0f);
                 audio.Play();
                 justSqueaked = true;
             }
         }
-        previousYRotation = currentYRotation;
+        previousRotation = currentRotation;
         
     }
 }
