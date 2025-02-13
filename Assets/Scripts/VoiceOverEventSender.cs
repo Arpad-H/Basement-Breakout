022a18487eaa8c0a4787422953f8fe2878d36a9b
leseeger <leon.seeger@hs-osnabrueck.de>
2025-02-13 15:16:35 +0100

    Implement event-driven voice-over system for interaction triggers
    - Added VoiceOverManager class:
      - Maps enum items to their respective AudioSources using a dictionary.
      - Subscribes to the static OnLeverAction event to play the correct audio clip.
      - Implements special-case handling to prevent duplicate playback for CHAINSAW/CANISTER and BOAT/REMOTECONTROLLERBOAT items.
    
    - Added VoiceOverEventSender class:
      - Detects trigger events via collisions or grabbable interactions.
      - Invokes the OnLeverAction event when the player interacts through either a collider or grab action.
      - Provides an enum to select between COLLIDER and GRABBABLE modes for flexible usage.
    
    This commit integrates a modular and efficient voice-over event system that informs lever interactions with appropriate audio feedback.

---
+++ Assets/Scripts/VoiceOverEventSender.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Interaction;
+using UnityEngine;
+
+public class VoiceOverEventSender : MonoBehaviour
+{
+    [SerializeField] public VoiceOverManager.item item;
+    [SerializeField] public ColliderOrGrabbable type;
+    private GameObject _targetCollider;
+    private bool _wasPlayed = false;
+    
+    public static event Action<VoiceOverManager.item> OnLeverAction;
+
+
+    private void Start()
+    {
+        _targetCollider = GameObject.Find("Player");
+    }
+
+    public enum ColliderOrGrabbable
+    {
+        GRABBABLE, COLLIDER
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (type == ColliderOrGrabbable.COLLIDER && _wasPlayed == false && other.gameObject == _targetCollider)
+        {
+            OnLeverAction?.Invoke(item);
+            _wasPlayed = true;
+        }
+    }
+
+    public void OnGrab()
+    {
+        if (type == ColliderOrGrabbable.GRABBABLE && _wasPlayed == false)
+        {
+           OnLeverAction?.Invoke(item);
+           _wasPlayed = true;
+        }
+    }
+    
+    
+    
+}
