7572c14d33732c52199d0ecd573f5e4c45ddf885
leseeger <leon.seeger@hs-osnabrueck.de>
2025-02-13 17:46:49 +0100

    fix ElectricictyManager.cs sparks

--- Assets/Scripts/ElectricictyManager.cs
+++ Assets/Scripts/ElectricictyManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Video;
 
