1731115795cb4facd579ccc02814ee48aed4e1ba
HSOSOli <oliver.snopko@hs-osnabrueck.de>
2025-02-13 16:39:07 +0100

    Outline Manager: no outlines when not in level

---
+++ Assets/Scripts/OutlineManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineManager : MonoBehaviour
+{
+    private GameObject[] gameObjects;
+    private Outline[] outlines;
+
+    void Awake()
+    {
+        GameManager.OnGameStateChanged += showOutlines;
+    }
+    
+    void Start()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("Outline");
+        outlines = new Outline[gameObjects.Length];
+        int i = 0;
+        foreach(GameObject gameObject in gameObjects)
+        {
+            if (gameObject.GetComponent<Outline>() != null)
+            {
+                outlines[i] = gameObject.GetComponent<Outline>();
+                outlines[i].enabled = false;
+                ++i;
+            }
+        }
+    }
+
+    private void enable()
+    {
+        for (int i = 0; i < outlines.Length; ++i)
+        {
+            outlines[i].enabled = true;
+        }
+    }
+
+    private void disable()
+    {
+        for (int i = 0; i < outlines.Length; ++i)
+        {
+            outlines[i].enabled = false;
+        }
+    }
+    
+    private void showOutlines(GameManager.GameState state)
+    {
+        if (state == GameManager.GameState.Tutorial)
+        {
+            enable();
+        }
+        else if(state != GameManager.GameState.Tutorial || state == GameManager.GameState.Game)
+        {
+            disable();
+        }
+    }
+}
